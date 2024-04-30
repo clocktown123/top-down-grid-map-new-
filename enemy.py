bddd025d22a630d7c3d755cd171fb075cbd2a4b3
clocktown123 <146383205+clocktown123@users.noreply.github.com>
2024-04-30 14:31:27 -0600

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from pygame.math import Vector2
 
 A = 0
 D = 1
@@ -9,18 +10,20 @@ S = 3
 
 class Enemy:
     def __init__ (self):
-        self.xpos = 400
-        self.ypos = 200
+        self.pos = Vector2(400,200)
         self.direction = D
         self.isAlive = True
     
-    def die(self, ballx, bally):
-        if math.sqrt((self.xpos-ballx)**2 + (self.ypos-bally)**2) <= 20:
+    def die(self, ballx, bally, cleavex, cleavey):
+        if math.sqrt((self.pos.x-ballx)**2 + (self.pos.y-bally)**2) <= 20:
+            self.isAlive = False
+
+        if math.sqrt((self.pos.x-cleavex)**2 + (self.pos.y-cleavey)**2) <= 20:
             self.isAlive = False
     
     def draw(self, screen):
         if self.isAlive == True:
-            pygame.draw.rect(screen, (20, 20, 40), ( self.xpos, self.ypos, 20, 20))
+            pygame.draw.rect(screen, (20, 20, 40), ( self.pos.x, self.pos.y, 20, 20))
 
     def move(self, map, ticker, px, py):
         if ticker % 40 == 0: #change this number to make him change direction less or more often
@@ -34,39 +37,39 @@ class Enemy:
             elif num == 3:
                 self.direction = S
         #check if the player is in the line of sight
-        if abs(int(py/50) - int(self.ypos/50))<2: #check that player and enemy are in the same row
-            if px < self.xpos: #check that player is to the left of the enemy
-                self.xpos -=1
+        if abs(int(py/50) - int(self.pos.y/50))<2: #check that player and enemy are in the same row
+            if px < self.pos.x: #check that player is to the left of the enemy
+                self.pos.x -=1
                 self.direction = A
             else:
-                self.xpos+=1
+                self.pos.x+=1
                 self.direction = D
-        if abs(int(px/50) - int(self.xpos/50))<2:
-            if py < self.ypos: #check that player is to the left of the enemy
-                self.ypos -=1
+        if abs(int(px/50) - int(self.pos.x/50))<2:
+            if py < self.pos.y: #check that player is to the left of the enemy
+                self.pos.y -=1
                 self.direction = W
             else:
-                self.ypos+=1
+                self.pos.y+=1
                 self.direction = S
         
-        if self.direction == D and map[int((self.ypos) / 50)][int((self.xpos + 20) / 50)] == 2:
+        if self.direction == D and map[int((self.pos.y) / 50)][int((self.pos.x + 20) / 50)] == 2:
             self.direction = W
-            self.xpos -= 6
-        if self.direction == A and map[int((self.ypos) / 50)][int((self.xpos - 20) / 50)] == 2:
+            self.pos.x -= 6
+        if self.direction == A and map[int((self.pos.y) / 50)][int((self.pos.x - 20) / 50)] == 2:
             self.direction = S
-            self.xpos += 6
-        if self.direction == S and map[int((self.ypos + 20) / 50)][int((self.xpos) / 50)] == 2:
+            self.pos.x += 6
+        if self.direction == S and map[int((self.pos.y + 20) / 50)][int((self.pos.x) / 50)] == 2:
             self.direction = A
-            self.ypos -= 6
-        if self.direction == W and map[int((self.ypos - 20) / 50)][int((self.xpos) / 50)] == 2:
+            self.pos.y -= 6
+        if self.direction == W and map[int((self.pos.y - 20) / 50)][int((self.pos.x) / 50)] == 2:
             self.direction = D
-            self.ypos += 6
+            self.pos.y += 6
 
         if self.direction == D:
-            self.xpos += 3
+            self.pos.x += 3
         elif self.direction == A:
-            self.xpos -= 3
+            self.pos.x -= 3
         elif self.direction == W:
-            self.ypos -=3
+            self.pos.y -=3
         elif self.direction == S:
-            self.ypos +=3
+            self.pos.y +=3
