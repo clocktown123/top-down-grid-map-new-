57e689a973bf624e2820e6a656370c81676fb9c2
clocktown123 <146383205+clocktown123@users.noreply.github.com>
2024-04-30 14:31:11 -0600

    Update limitless.py

--- limitless.py
+++ limitless.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import random
+from pygame.math import Vector2
 
 LEFT = 0
 RIGHT = 1
@@ -11,14 +12,13 @@ W = 5
 
 class fireball:
     def __init__(self):
-        self.xpos = -10 # draw off scren when not in use
-        self.ypos = -10
+        self.pos = Vector2(-10,-10)
         self.isAlive = False
         self.direction = RIGHT
 
     def shoot(self, x, y, dir):
-        self.xpos = x + 20 # start fireball at center of player
-        self.ypos = y +20
+        self.pos.x = x + 20 # start fireball at center of player
+        self.pos.y = y +20
         self.isAlive = True
         self.direction = dir
 
@@ -26,13 +26,13 @@ class fireball:
 
     def move(self, dir):
         if self.direction == RIGHT:
-            self.xpos+=20
+            self.pos.x+=20
         elif self.direction == LEFT:
-            self.xpos-=20
+            self.pos.x-=20
         if self.direction == DOWN:
-            self.ypos+=20
+            self.pos.y+=20
         elif self.direction == UP:
-            self.ypos-=20
+            self.pos.y-=20
         
     def draw(self,screen):
         self.rando = random.randrange(0, 2)
@@ -43,18 +43,18 @@ class fireball:
         else:
             self.blueAlive = True
         if self.redAlive:
-            pygame.draw.circle(screen, (220,20,60), (self.xpos, self.ypos), 10)
-            pygame.draw.circle(screen, (250,0,0), (self.xpos, self.ypos), 5)
+            pygame.draw.circle(screen, (220,20,60), (self.pos.x, self.pos.y), 10)
+            pygame.draw.circle(screen, (250,0,0), (self.pos.x, self.pos.y), 5)
         if self.blueAlive:
-            pygame.draw.circle(screen, (0,0,225), (self.xpos, self.ypos), 10)
-            pygame.draw.circle(screen, (65,105,255), (self.xpos, self.ypos), 5)
+            pygame.draw.circle(screen, (0,0,225), (self.pos.x, self.pos.y), 10)
+            pygame.draw.circle(screen, (65,105,255), (self.pos.x, self.pos.y), 5)
     
     def B_draw(self, screen):
-        pygame.draw.circle(screen, (138,43,226), (self.xpos, self.ypos), 30)
-        pygame.draw.circle(screen, (147,112,219), (self.xpos, self.ypos), 15)
+        pygame.draw.circle(screen, (138,43,226), (self.pos.x, self.pos.y), 30)
+        pygame.draw.circle(screen, (147,112,219), (self.pos.x, self.pos.y), 15)
 
     def collide(self, x, y):
-        if math.sqrt((self.xpos - x) **2 + (self.ypos - y) **2) < 25:
+        if math.sqrt((self.pos.x - x) **2 + (self.pos.y - y) **2) < 25:
             print("collision")
             return True
         else:
